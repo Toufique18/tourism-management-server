f239749bca1405da95aaf76900a47852b6e92417
Toufique18 <toufiqueeamam7@gmail.com>
2024-04-28 16:05:39 +0600

    successfully data added to database and commit

--- index.js
+++ index.js
@@ -6,14 +6,9 @@ require('dotenv').config()
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-
-//toufique-tourism
-//qJb5Z9EGTSxDVJTy
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mhvsuxa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-console.log(uri)
+//console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -27,12 +22,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const spotsCollection =client.db('touristsSpotsDB').collection('information');
+
+
+
+    app.post("/information", async (req, res)=> {
+        const newInformation = req.body;
+        console.log(newInformation)
+        const result = await spotsCollection.insertOne(newInformation)
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
